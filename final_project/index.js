e6357b4fc0675c31364ff033876de1d68ecbc724
ALAA-HISHAM <alaa.hisham45@gmail.com>
2025-01-18 06:05:49 -0500

    Implemented routes for fetching books and reviews

--- final_project/index.js
+++ final_project/index.js
@@ -10,9 +10,28 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+app.use("/customer/auth/*", function auth(req, res, next) {
+        // Check if the user is authenticated via session
+        // Check if there's a JWT token in the request headers
+    console.log(req.session)
+            if(req.session){
+                if(req.session.authorization){
+                        let token = req.session.authorization.token;
+                        jwt.verify(token, 'my-secret-key', (err, user) => {
+                            if (err) {
+                                // Token verification failed
+                                res.status(401).json({ error: 'galek err' });
+                            } else {
+                                req.session.user = user;
+                                next();
+                            }
+                        });
+                }
+                else{
+                    res.status(401).json({ error: 'Unauthorized here' });
+                }
+            }
+        });
  
 const PORT =5000;
 
